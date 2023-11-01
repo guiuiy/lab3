9d0914aab3f8f2f75583355b872da1fa758b727a
guiuiy <bblu2981@gmail.com>
2023-11-01 00:52:18 -0700

    did lab3

--- LinkedListTests.java
+++ LinkedListTests.java
@@ -3,13 +3,23 @@ import org.junit.*;
 
 public class LinkedListTests {
 
+
     @Test
-    public void appendTest() {
+    public void appendTest1() {
+        LinkedList test = new LinkedList();
+        test.append(1);
+        test.append(2);
+        assertEquals("1 2 " , test.toString());
+    }
+
+    @Test
+    public void appendTest2() {
         LinkedList test = new LinkedList();
         test.append(1);
         test.append(2);
         test.append(3);
         assertEquals("1 2 3 " , test.toString());
     }
+
  
 }
